713ef6cc58185f465b6c9d22c68d35d7f59897f5
MD Alimuzzaman Haris <73691596+Az-Haris@users.noreply.github.com>
2022-01-27 01:37:24 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main()
+{
+    int a;
+
+    for(a=1; a>0; a=a+2)
+    {
+        printf("\t%d\n",a);
+    }
+
+
+    getch();
+    return 0;
+}
